23a2b6f48c87ba13163612a11ffa98a647847b04
Saad Karkal <sakarkal@e3r8p18.1337.ma>
2023-08-19 19:21:28 +0100

    A - Increase by 2

---
+++ Day00/A.cpp
@@ -0,0 +1,22 @@
+// A - Increase by 2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x >= 0)
+            cout << x + 2;
+        else
+            cout << x;
+        if (i != n - 1)
+            cout << ' ';
+    }
+    cout << endl;
+}
