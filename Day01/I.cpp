018d6aab2daaf04d2aa092cae5053a5fe1485119
Saad Karkal <Karkalsaad@gmail.com>
2023-08-19 19:58:48 +0100

    I - Two Sets

--- Day01/I.cpp
+++ Day01/I.cpp
@@ -8,36 +8,41 @@
 
 using namespace std;
 
-void prSet(set<int> &st) {
+void __Set(set<int> &st)
+{
 	cout << st.size() << '\n';
 	for (int i: st)
 		cout << i << ' ';
 	cout << '\n';
 }
 
-int main() {
-	long long n; cin>> n;
+int main()
+{
+	long long   n;
+	long long s = 0;
 
+    cin >> n;
 	long long sum = (n * (n + 1)) / 2;
 
-	if (sum % 2 == 1) {
+	if (sum % 2 == 1)
+    {
 		cout << "NO\n";
 		return 0;
 	}
 
-	long long s = 0;
-	set<int> a, b;
 
-	for (int i = n; i >= 1; i--) {
-		if (s + i > sum / 2) {
+	set<int> a, b;
+	for (int i = n; i >= 1; i--)
+    {
+		if (s + i > sum / 2)
 			b.insert(i);
-		} else {
+        else
+        {
 			s += i;
 			a.insert(i);
 		}
 	}
-
 	cout << "YES\n";
-	prSet(a);
-	prSet(b);
+	__Set(a);
+	__Set(b);
 }
