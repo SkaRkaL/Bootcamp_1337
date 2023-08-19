41ca6e6830570133796e51624dc53615ee4d462a
Saad Karkal <Karkalsaad@gmail.com>
2023-08-19 19:56:18 +0100

    I - Two Sets

---
+++ Day01/I.cpp
@@ -0,0 +1,43 @@
+// I - Two Sets
+
+#include <iostream>
+#include <set>
+#include <stack>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void prSet(set<int> &st) {
+	cout << st.size() << '\n';
+	for (int i: st)
+		cout << i << ' ';
+	cout << '\n';
+}
+
+int main() {
+	long long n; cin>> n;
+
+	long long sum = (n * (n + 1)) / 2;
+
+	if (sum % 2 == 1) {
+		cout << "NO\n";
+		return 0;
+	}
+
+	long long s = 0;
+	set<int> a, b;
+
+	for (int i = n; i >= 1; i--) {
+		if (s + i > sum / 2) {
+			b.insert(i);
+		} else {
+			s += i;
+			a.insert(i);
+		}
+	}
+
+	cout << "YES\n";
+	prSet(a);
+	prSet(b);
+}
